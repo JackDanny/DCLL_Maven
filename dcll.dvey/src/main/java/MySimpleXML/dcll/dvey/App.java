3fc32be144dcd5190db23f5acb2eb9d580099374
Wedge <wedg@hotmail.fr>
2013-02-21 08:57:39 +0100

    premier commit

---
+++ dcll.dvey/src/main/java/MySimpleXML/dcll/dvey/App.java
@@ -0,0 +1,13 @@
+package MySimpleXML.dcll.dvey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
