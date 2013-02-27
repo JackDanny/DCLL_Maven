47b0d96b9170f91196797af93d2c70dabbcba984
Wedge <wedg@hotmail.fr>
2013-02-27 14:15:16 +0100

    #1commit1

--- dcll.dvey/src/main/java/MySimpleXML/dcll/dvey/App.java
+++ dcll.dvey/src/main/java/MySimpleXML/dcll/dvey/App.java
@@ -1,13 +1,31 @@
 package MySimpleXML.dcll.dvey;
 
+import java.io.IOException;
+
+import nu.xom.*;
+import nu.*;
+import org.w3c.dom.Element;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws ValidityException, ParsingException, IOException {
+    	
+    	//root.getAttribute("root");
+    	//System.out.println("aha");
+    	Builder builder = new Builder();
+    	Document doc=builder.build("pom.xml");
+    	System.out.println(doc.toXML());
+    	System.out.println("wahou");
+    	
+    	
+    
+    	
+    	
+    	
+    	
     }
 }
